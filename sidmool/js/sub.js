0ae53d2d33675334d977cf8c60eeeb59a24ec70b
jar3543 <34150355+jar3543@users.noreply.github.com>
2018-03-28 19:53:45 +0900

    시드물 상세페이지 보완

--- sidmool/js/sub.js
+++ sidmool/js/sub.js
@@ -2,27 +2,34 @@
 $(function(){
   $('#box1').click(function(){
     $('html, body').stop().animate({
-      scrollTop:'-=100'
+      scrollTop:'1100'
     },1000,'easeInOutExpo');
-    console.log('box1')
   })
-
-  // $('#box2').click(function(){
-  //   $('html, body').stop().animate({
-  //     scrollTop:'+=100'
-  //   },1000);
-  // })
 
   $('#box3').click(function(){
     $('html, body').stop().animate({
-      scrollTop:'0'
-    },1000);
+      scrollTop:'8950'
+    },1000,'easeInOutExpo');
   })
 
   $('#box4').click(function(){
     $('html, body').stop().animate({
-      scrollTop:$(document).height()
-    },1000);
+      scrollTop:'9550'
+    },1000,'easeInOutExpo');
   })
+
+  // 필독공지 모달 팝업 열기/닫기
+  $('#btn-modal').click(function(){
+    $('#myModal2').modal('toggle');
+  })
+  $('#btn-modal-close').click(function(){
+    $('#myModal2').modal('hide');
+  })
+
+  // 리뷰 게시판 툴팁 호출
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
 
 })
