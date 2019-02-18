e36468e9d26b4d537b40cb5199546d04cf6ae3b0
jar3543 <34150355+jar3543@users.noreply.github.com>
2019-02-18 14:00:21 +0900

    update portfolio

--- AreumJeon/js/loading.js
+++ AreumJeon/js/loading.js
@@ -1,6 +1,17 @@
-  //로딩이미지 처리
+//로딩이미지 처리
   $(window).on('load',function(){
 
     $('.bg').fadeOut(2000);
 
   });
+
+
+
+// 로딩이미지가 사라지고 maintext (7초뒤에) 띄우기
+  $(function(){
+    $(window).on('load',function(){
+      $('.maintext1').animate({
+        opacity:1
+      },7000);
+    })
+  })
--- AreumJeon/js/star.js
+++ AreumJeon/js/star.js
@@ -35,13 +35,3 @@ function starMove(){
   }
 }
 //star end
-
-
-// 로딩이미지가 사라지고 maintext (7초뒤에) 띄우기
-$(function(){
-  $(window).on('load',function(){
-    $('.maintext').animate({
-      opacity:1
-    },7000);
-  })
-})
